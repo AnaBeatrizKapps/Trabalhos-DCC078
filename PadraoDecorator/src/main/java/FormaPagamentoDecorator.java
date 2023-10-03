d78dd782b84b97df15c8561dcf63e786dfe67cf3
Ana <anabeatrizkapps@ice.ufjf.br>
2023-10-03 17:24:14 -0300

    Padrao Decorator

---
+++ PadraoDecorator/src/main/java/FormaPagamentoDecorator.java
@@ -0,0 +1,38 @@
+public abstract class FormaPagamentoDecorator implements IFormaPagamento {
+
+    private IFormaPagamento formaPagamento;
+    public String estrutura;
+
+    public FormaPagamentoDecorator(IFormaPagamento formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public IFormaPagamento getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(IFormaPagamento formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public abstract float getAumento();
+
+    public float getValor() {
+        return this.formaPagamento.getValor() + getAumento();
+    }
+
+    public float getValorInicial(){
+        return this.formaPagamento.getValorInicial();
+    }
+
+    public abstract String getNomeEstrutura();
+
+    public String getEstrutura() {
+        return this.formaPagamento.getEstrutura() + "/" + this.getNomeEstrutura();
+    }
+
+    public void setEstrutura(String estrutura) {
+        this.estrutura = estrutura;
+    }
+}
+
